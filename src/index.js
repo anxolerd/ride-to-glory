e8430c94d0ea97e63b3c64bc535d76ec6443fcb9
Oleksandr Kovalchuk <anxolerd@outlook.com>
2022-04-20 12:36:24 +0300

    Build gpx xml for the track with new timestamps
    Implement logic to reconstruct the gpx file with newer timestamps

---
+++ src/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const xmlbuilder = require('xmlbuilder2');
+const trackpoints = require('./trackpoints.js');
+
+const now = new Date();
+
+let root = xmlbuilder.create({ version: '1.0', encoding: "UTF-8"  })
+    .ele('gpx')
+        .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+        .att('xmlns', 'http://www.topografix.com/GPX/1/1')
+        .att('xsi:schemaLocation', 'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd http://www.topografix.com/GPX/gpx_style/0/2 http://www.topografix.com/GPX/gpx_style/0/2/gpx_style.xsd')
+        .att('xmlns:gpxtpx', 'http://www.garmin.com/xmlschemas/TrackPointExtension/v1')
+        .att('xmlns:gpxx', 'http://www.garmin.com/xmlschemas/GpxExtensions/v3')
+        .att('xmlns:gpx_style', 'http://www.topographix.com/GPX/gpx_style/0/2')
+        .att('version', '1.1')
+        .ele('metadata')
+            .ele('name')
+                .txt('')
+            .up()
+        .up()
+        .ele('trk')
+            .ele('name')
+                .txt('TODO')
+            .up()
+            .ele('type')
+                .txt('Cycling')
+            .up()
+            .ele('trkseg');
+
+trackpoints.forEach((p) => {
+    root = root.ele('trkpt')
+        .att('lat', p.lat)
+        .att('lon', p.lng)
+        .ele('ele')
+            .txt(p.elevation)
+            .up()
+        .ele('time')
+            .txt((new Date(now.getTime() + (Number(p.timedelta_micro) / 1000))).toISOString())
+            .up()
+        .up()
+});
+root = root
+            .up()
+        .up()
+    .up()
+    .end({pretty: true});
+console.log(root.toString());
